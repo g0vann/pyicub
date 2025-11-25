4b1fa3948e86af0a5715b2adef6e20d6a300c61f
Papuzzitas-97 <papuzzitas-97@hotmail.it>
2025-11-25 19:57:53 +0100

    Add docker configuration files to .gitignore and untrack them

--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source "$(dirname "$0")/setup.sh"
-
-LOCAL_USER_UID=$(id -u) LOCAL_USER_GID=$(id -g) docker compose up
-
-LOCAL_USER_UID=$(id -u) LOCAL_USER_GID=$(id -g) docker compose down --remove-orphans
